4c0a232891db0aef98c76e386f0b83d3fcaf15a3
Gus Ribeiro <gustavo.montes@unesp.br>
2022-07-24 19:19:42 -0300

    movimento

--- Jogo.java
+++ Jogo.java
@@ -9,14 +9,15 @@ class Jogo extends JFrame{
     final int FUNDO = 0;
     final int PRETO = 1;
     final int AZUL = 2;
-    int estado = AZUL;
+    final int COME = 3;
+    int estado = AZUL; 
 
 
     Image img[] = new Image[20];
     int posX = 10;
     Timer timer;
     Desenho des = new Desenho();
-
+    
     class Desenho extends JPanel{
 
         Desenho(){
@@ -25,6 +26,7 @@ class Jogo extends JFrame{
                 img[FUNDO] = ImageIO.read(new File("fundo.jpg"));
                 img[PRETO] = ImageIO.read(new File("puffle.png"));
                 img[AZUL] = ImageIO.read(new File("puffle_azul.png"));
+                // img[COME] = ImageIO.read(new File("azul_come.jpg"));
             } catch(IOException e){
                 JOptionPane.showMessageDialog(this, "Erro no carregamento da imagem\n"+e, "Erro", JOptionPane.ERROR_MESSAGE);
                 System.exit(1);
@@ -34,9 +36,46 @@ class Jogo extends JFrame{
         public void paintComponent(Graphics g){
             super.paintComponent(g);
             g.drawImage(img[FUNDO], 0, 0, getSize().width, getSize().height, this);
-            g.drawImage(img[AZUL], posX, getSize().height - img[AZUL].getHeight(this) - 10, this);
-            g.drawImage(img[PRETO], posX, getSize().height - img[AZUL].getHeight(this) - 10, this);
+            g.drawImage(img[estado], posX, getSize().height - img[estado].getHeight(this) - 10, this);
+            g.drawImage(img[PRETO], posX+700, getSize().height - img[AZUL].getHeight(this) - 10, this);
             Toolkit.getDefaultToolkit().sync();    
+        }
+    }
+
+    void andaPDireita(){
+        if(estado == COME){
+            estado = AZUL;
+        }
+        posX += 10;
+        repaint();
+    }
+    void andaPEsquerda(){
+        if(estado == COME){
+            estado = AZUL;
+        }
+        posX -= 10;
+        repaint();
+    }
+    // void comeFruta(){
+    //     if(estado == AZUL){
+    //         estado = COME;
+    //     }
+    // }
+
+    class TrataTeclas extends KeyAdapter{ // classe que trata o movimento das teclas
+        public void KeyPressed(KeyEvent e){
+            if(e.getKeyCode() == KeyEvent.VK_RIGHT) { // Tecla '->'
+                estado = AZUL;
+                andaPDireita();
+            }
+            else if(e.getKeyCode() == KeyEvent.VK_LEFT){ // Tecla '<-'
+                estado = AZUL;
+                andaPEsquerda();
+            }
+            // else if(e.getKeyCode() == KeyEvent.VK_S){ // Tecla 'S'
+            //     estado = COME;
+            //     comeFruta();
+            // }
         }
     }
 
@@ -46,6 +85,15 @@ class Jogo extends JFrame{
         add(des);
         pack();
         setVisible(true);
+        addKeyListener(new TrataTeclas()); // classe que trata os eventos ligados às teclas
+        new Timer(100, new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent ae){
+                andaPDireita();
+                andaPEsquerda();
+                // comeFruta();
+            }
+        }).start();
     }
 
     static public void main(String args[]){
