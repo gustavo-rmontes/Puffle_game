e69785b2d1ab9829a5f79293bb448c1a3cd4f6d1
Gus Ribeiro <gustavo.montes@unesp.br>
2022-07-25 15:29:37 -0300

    s

--- Jogo.java
+++ Jogo.java
@@ -4,6 +4,7 @@ import java.awt.image.*;
 import javax.swing.*;
 import java.io.*;
 import javax.imageio.*;
+import java.util.Random;
 
 class Jogo extends JFrame{
     // Constantes para o vetor de imagens a serem inseridas
@@ -11,6 +12,7 @@ class Jogo extends JFrame{
     final int PRETO = 1;
     final int AZUL = 2;
     final int COME = 3;
+    final int COMIDA = 4;
 
     int estado = AZUL; // controla as transições de imagem do jogador 1
     int posX = 10; // controla a posição horizontal do jogador
@@ -19,6 +21,8 @@ class Jogo extends JFrame{
     Image img[] = new Image[20];
     Timer timer;
     Desenho des = new Desenho();
+
+    boolean jogoContinua = true;
     
     class Desenho extends JPanel{
 
@@ -26,6 +30,7 @@ class Jogo extends JFrame{
             try{
                 setPreferredSize(new Dimension(828, 467));
                 img[FUNDO] = ImageIO.read(new File("fundo.jpg"));
+                img[COMIDA] = ImageIO.read(new File("comida"));
                 img[PRETO] = ImageIO.read(new File("puffle.png"));
                 img[AZUL] = ImageIO.read(new File("puffle_azul.png"));
                 img[COME] = ImageIO.read(new File("azul_come.jpg"));
@@ -38,10 +43,18 @@ class Jogo extends JFrame{
         public void paintComponent(Graphics g){
             super.paintComponent(g);
             g.drawImage(img[FUNDO], 0, 0, getSize().width, getSize().height, this);
+            g.drawImage(img[COMIDA], posicaoAleatoria(), posicaoAleatoria(), getSize().width, getSize().height, this);
             g.drawImage(img[estado], posX, getSize().height - img[estado].getHeight(this) - 10, this);
             g.drawImage(img[PRETO], posXPlayer2, getSize().height - img[PRETO].getHeight(this) - 10, this);
             Toolkit.getDefaultToolkit().sync();    
         }
+    }
+    
+    // Gera um inteiro aleatório entre 0 e 800  
+    int posicaoAleatoria(){
+        Random nAleatorio = new Random();
+        int n = nAleatorio.nextInt(800);
+        return n;
     }
 
     void andaPDireita(){
@@ -70,12 +83,18 @@ class Jogo extends JFrame{
             else if(e.getKeyCode() == KeyEvent.VK_LEFT){ // Tecla '<-'
                 andaPEsquerda();
             }
-            else if(e.getKeyCode() == KeyEvent.VK_A){ // Tecla 'A'
+            else if(e.getKeyCode() == KeyEvent.VK_C){ // Tecla 'C'
                 comeFruta();
             }
         }
     }
 
+    class Comida extends Thread{
+        public void run(){
+            
+        }
+    }
+    
     Jogo(){
         super("Puffle");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -83,18 +102,10 @@ class Jogo extends JFrame{
         pack();
         setVisible(true);
         addKeyListener(new TrataTeclas()); // classe que trata os eventos ligados às teclas
-        // timer = new Timer(100, new ActionListener(){
-        //     @Override
-        //     public void actionPerformed(ActionEvent ae){
-        //         andaPDireita();
-        //         andaPEsquerda();
-        //         comeFruta();
-        //     }
-        // });
     }
 
     static public void main(String args[]){
-        Jogo a = new Jogo();
+        new Jogo();
     }
 
 }
